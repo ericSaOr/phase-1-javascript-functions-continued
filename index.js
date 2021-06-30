9bf48fdabe6d451ca91a49f5de6db24bd66e6be5
Eric Sanchez <ericfsod@gmail.com>
2021-06-29 22:21:42 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Your code here
+
+function saturdayFun (str = 'roller-skate'){
+    
+        return `This Saturday, I want to ${str}!`;
+    }
+
+saturdayFun('bathe my dog');
+
+const mondayWork = function(str = "go to the office"){
+    return `This Monday, I will ${str}.`
+}
+
+function wrapAdjective (flair = "*"){
+    return function innerFunction (str = "special"){
+        return `You are ${flair}${str}${flair}!`
+    } 
+    
+    
+    
+       
+}
+
